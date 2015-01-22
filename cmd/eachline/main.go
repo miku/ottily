5ca49b0cfec136b685b93399f24c17f12a4974c5
Martin Czygan <martin.czygan@gmail.com>
2015-01-22 17:15:02 +0100

    add valdis and eachline commands

---
+++ cmd/eachline/main.go
@@ -0,0 +1,48 @@
+// This will leak memory
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+
+	flag.Parse()
+
+	ff, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ff.Close()
+	reader := bufio.NewReader(ff)
+
+	start := time.Now()
+	counter := 0
+
+	// var container map[string]interface{}
+
+	for {
+		_, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		// json.Unmarshal([]byte(line), &container)
+		counter++
+		if counter%100000 == 0 {
+			elapsed := time.Since(start)
+			speed := float64(counter) / elapsed.Seconds()
+			eta, _ := time.ParseDuration(fmt.Sprintf("%0.3fs", float64(70000000)/speed))
+			log.Printf("%d %0.3f %s\n", counter, speed, eta)
+		}
+	}
+
+}
--- cmd/valdis/main.go
+++ cmd/valdis/main.go
@@ -1,14 +1,57 @@
+// This will leak memory
 package main
 
-import "github.com/idada/v8.go"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+
+	"github.com/idada/v8.go"
+)
 
 func main() {
-	engine := v8.NewEngine()
-	script := engine.Compile([]byte("'Hello ' + 'World!'"), nil)
-	context := engine.NewContext(nil)
 
-	context.Scope(func(cs v8.ContextScope) {
-		result := cs.Run(script)
-		println(result.ToString())
-	})
+	flag.Parse()
+
+	ff, err := os.Open(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ff.Close()
+	reader := bufio.NewReader(ff)
+
+	vm := v8.NewEngine()
+	script := vm.Compile([]byte("input.length"), nil)
+	context := vm.NewContext(nil)
+
+	counter := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		line = strings.TrimSpace(line)
+		context.Scope(func(cs v8.ContextScope) {
+			s := vm.Compile([]byte(fmt.Sprintf("var input = %s", strconv.Quote(line))), nil)
+			cs.Run(s)
+			result := cs.Run(script)
+			fmt.Println(result.ToString())
+		})
+		counter++
+		if counter%100000 == 0 {
+			log.Println("Running GC.")
+			runtime.GC()
+			log.Println("Done.")
+		}
+	}
+
 }
